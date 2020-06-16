8ae22f7f17e26c868775cf4f251db63d5ebfd7c2
Matthew Talluto <mtalluto@gmail.com>
2020-06-16 15:05:35 +0200

    Updated publications page

--- pubs/make-page.zsh
+++ pubs/make-page.zsh
@@ -4,7 +4,7 @@ cat bib/*.bib > pubs.bib
 pandoc pubs.tex --from=latex --to=html  --filter=pandoc-citeproc --bibliography=pubs.bib \
     --output=pubs_body.html --csl=pubs.csl
 
-cat head.html pubs.html foot.html > ../pubs.html
+cat head.html pubs_body.html foot.html > ../pubs.html
 
 ## convert ris 2 bib
 ## ris2xml <risfile.ris> | xml2bib > <output.bib>
